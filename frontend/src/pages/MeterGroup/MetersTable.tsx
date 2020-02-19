b974abd9288f18b90690a5ad7dd5dc39b53a9574
Milo Toor <milo.toor@gmail.com>
2020-02-19 12:19:36 -0800

    Remove columns

--- frontend/src/pages/MeterGroup/MetersTable.tsx
+++ frontend/src/pages/MeterGroup/MetersTable.tsx
@@ -3,7 +3,7 @@ import { createUseStyles } from 'react-jss';
 
 import * as api from '@nav/shared/api';
 import { Fade, Progress, Table, Typography } from '@nav/shared/components';
-import { getComputedValue, Meter, MeterGroup } from '@nav/shared/models/meter';
+import { Meter, MeterGroup } from '@nav/shared/models/meter';
 import { Theme } from '@nav/shared/styles';
 
 
@@ -59,18 +59,14 @@ const MetersTable: React.FC<MetersTableProps> = ({ meterGroupId }) => {
             <Table.Head>
               <Table.Row>
                 <Table.Cell>SA ID</Table.Cell>
-                <Table.Cell align="right">Rate Plan</Table.Cell>
-                <Table.Cell align="right">Total kWh</Table.Cell>
-                <Table.Cell align="right">Maximum kW</Table.Cell>
+                <Table.Cell>Rate Plan</Table.Cell>
               </Table.Row>
             </Table.Head>
             <Table.Body>
               {meters.map(meter =>
                 <Table.Row key={meter.id}>
                   <Table.Cell useTh>{meter.metaData.saId}</Table.Cell>
-                  <Table.Cell align="right">{meter.metaData.ratePlan}</Table.Cell>
-                  <Table.Cell align="right">abcd</Table.Cell>
-                  <Table.Cell align="right">{getMaxKw(meter)}</Table.Cell>
+                  <Table.Cell>{meter.metaData.ratePlan}</Table.Cell>
                 </Table.Row>
               )}
             </Table.Body>
@@ -83,13 +79,6 @@ const MetersTable: React.FC<MetersTableProps> = ({ meterGroupId }) => {
       }
     </div>
   );
-  
-  function getMaxKw (meter: Meter) {
-    const maxKw = getComputedValue(meter, 'maxKw');
-    return typeof maxKw === 'number'
-      ? parseFloat(maxKw.toFixed(3))
-      : '-';
-  }
 };
 
 /** ============================ Exports =================================== */
