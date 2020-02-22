eb0145c9891c83e1a0b9df0b877451b1b787f657
Milo Toor <milo.toor@gmail.com>
2020-02-21 17:38:17 -0800

    Create `UploadPage` component

---
+++ frontend/src/pages/Upload/UploadPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { AppContainer, Typography } from '@nav/shared/components';
+
+
+/** ============================ Components ================================ */
+const UploadPage: React.FC = () => {
+  return (
+    <AppContainer>
+      <Typography variant="h4">Upload</Typography>
+    </AppContainer>
+  );
+};
+
+/** ============================ Exports =================================== */
+export default UploadPage;
