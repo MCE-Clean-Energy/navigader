656c59fd7e78d1ad930bc7a431dd10742558c18f
Milo Toor <milo.toor@gmail.com>
2020-02-24 11:19:44 -0800

    Refactor FAB button implementation

--- frontend/src/shared/components/Button.tsx
+++ frontend/src/shared/components/Button.tsx
@@ -4,38 +4,34 @@ import MuiButton from '@material-ui/core/Button';
 import MuiFab from '@material-ui/core/Fab';
 import MuiIconButton from '@material-ui/core/IconButton';
 
-import { printWarning } from '@nav/shared/util';
-import { Icon, ValidIcon } from './Icon';
+import { Icon, IconProps } from './Icon';
 
 
 /** ============================ Types ===================================== */
 type BaseButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
   color?: 'primary' | 'secondary';
-  fab?: ValidIcon;
   icon?: boolean;
-} & Pick<BaseButtonProps, 'type'>
+} & Pick<BaseButtonProps, 'type'>;
+
+type FabProps = Omit<ButtonProps, 'type'> & IconProps;
+type Button = React.FC<ButtonProps> & {
+  Fab: React.FC<FabProps>
+};
 
 /** ============================ Components ================================ */
-export const Button: React.FC<ButtonProps> = ({ fab, icon, ...rest }) => {
-  if (fab) {
-    if (icon) {
-      // A button can't be a FAB and an icon button at the same time
-      printWarning(
-        '`Button component` received both `fab` and `icon` props. At most one prop' +
-        ' should be provided.'
-      );
-    }
-    
-    const fabProps = omit(rest, 'children');
-    return (
-      <MuiFab {...fabProps}>
-        <Icon name={fab} />
-      </MuiFab>
-    );
-  }
-  
+export const Button: Button = ({ icon, ...rest }) => {
   return icon
     ? <MuiIconButton {...rest} />
     : <MuiButton variant="contained" {...rest} />;
+};
+
+Button.Fab = ({ name, size, ...rest }) => {
+  const fabProps = omit(rest, 'children');
+  const iconProps = { name, size };
+  return (
+      <MuiFab {...fabProps}>
+        <Icon {...iconProps} />
+      </MuiFab>
+    );
 };
