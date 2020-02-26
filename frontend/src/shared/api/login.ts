e14a1b09d53082c0dbf176b3b37876eb302b1364
Milo Toor <milo.toor@gmail.com>
2020-02-26 11:21:21 -0800

    Save authorization token upon successful login

--- frontend/src/shared/api/login.ts
+++ frontend/src/shared/api/login.ts
@@ -1,6 +1,23 @@
+import { setCookie } from '@nav/shared/util';
 import { postRequest } from './util';
 
 
-export function login (username: string, password: string): Promise<Response> {
-  return postRequest('/login/rest-auth/login/', {  username, password });
+/** ============================ Types ===================================== */
+type LoginResponse = {
+  key: string;
+};
+
+/** ============================ API Methods =============================== */
+export async function login (username: string, password: string): Promise<Response> {
+  return postRequest('/beo/rest-auth/login/', {  username, password })
+    .then(res => {
+      res.json().then((response: LoginResponse) => {
+        if (res.status === 200) {
+          // Store the token
+          setCookie('authToken', response.key);
+        }
+      });
+      
+      return res;
+    });
 }
