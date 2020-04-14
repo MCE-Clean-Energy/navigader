0d09c487c76198d7211ac0343ed4c315bbe81b17
Milo Toor <milo.toor@gmail.com>
2020-04-14 09:59:04 -0700

    Invalidate CloudFront's cache after deploying the application (#25)

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 # Set up environment variables
 export REACT_APP_ENV=prod;
 export REACT_APP_BEO_URI=https://api.navigader.com;
@@ -10,3 +12,14 @@ npm run build;
 printf '\n\n';
 echo 'Deploying to S3...'
 aws s3 sync build/ s3://navigader.com
+
+# Invalidate CloudFront cache
+printf '\n\n';
+if [[ -v PROD_CLOUDFRONT_ID ]]; then
+  echo 'Invalidating dev CloudFront cache';
+  aws cloudfront create-invalidation --distribution-id $PROD_CLOUDFRONT_ID --paths "/*"
+else
+  echo 'WARNING: could not invalidate the CloudFront cache! Expected PROD_CLOUDFRONT_ID environment
+  variable to be set. The S3 bucket has been updated, but the cache may still be active.';
+fi
+
--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 # Set up environment variables
 export REACT_APP_ENV=staging;
 export REACT_APP_BEO_URI=https://api-staging.navigader.com;
@@ -10,3 +12,13 @@ npm run build;
 printf '\n\n';
 echo 'Deploying to S3...'
 aws s3 sync build/ s3://staging.navigader.com
+
+# Invalidate CloudFront cache
+printf '\n\n';
+if [[ -v STAGING_CLOUDFRONT_ID ]]; then
+  echo 'Invalidating dev CloudFront cache';
+  aws cloudfront create-invalidation --distribution-id $STAGING_CLOUDFRONT_ID --paths "/*"
+else
+  echo 'WARNING: could not invalidate the CloudFront cache! Expected STAGING_CLOUDFRONT_ID environment
+  variable to be set. The S3 bucket has been updated, but the cache may still be active.';
+fi
