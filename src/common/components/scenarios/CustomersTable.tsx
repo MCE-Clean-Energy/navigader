1927a6d2dcd0fa065dd2c8fe0cfc1fb7f4f37867
Dylan Grayson <dylan@terraverde.energy>
2020-12-21 16:32:29 -0500

    one liners

--- src/common/components/scenarios/CustomersTable.tsx
+++ src/common/components/scenarios/CustomersTable.tsx
@@ -106,8 +106,7 @@ export const CustomersTable: React.FC<CustomersTableProps> = (props) => {
                 column="Expenses Impact"
                 info={{
                   measuresImpact:
-                    'to overall expenses. Calculated as procurement expenses plus $/kW RA ' +
-                    'impacts',
+                    'to overall expenses. Calculated as procurement expenses plus $/kW RA impacts',
                   negativeMeans: 'overall expenses have gone down',
                   positiveMeans: 'overall expenses have gone up',
                 }}
